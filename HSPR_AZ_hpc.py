5773a74643ed712b258ccd866af18e1ea515b67e
6561S <132370281+6561S@users.noreply.github.com>
2024-04-24 12:05:42 +0100

    Update HSPR_AZ_hpc.py

--- HSPR_AZ_hpc.py
+++ HSPR_AZ_hpc.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # script: HSPR_AZ_tidy.py
-# author: Rituparna Goswami, Enrico Sandro Colizzi, Alexis Jiayi Zeng
 script_usage="""
 usage
     For de novo parameter setting: HSPR_AZ_hpc.py -nit <numberInteration> -wa2 <withA2> -mdn <modelVersion> [options]
